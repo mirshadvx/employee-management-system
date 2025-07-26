828f5901849c9c62d47022e673de46d9afec27ab
mirshad <mirshadkvx@gmail.com>
2025-07-26 21:56:59 +0530

    feat(client): initialized react app with tailwind css, added .gitignore

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <>
+            <div className="bg-black min-h-screen text-white text-2xl">test</div>
+        </>
+    );
+}
+
+export default App;
