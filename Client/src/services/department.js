5242383f16ab30ddb7c0a80c751acd7d3973e027
mirshad <mirshadkvx@gmail.com>
2025-07-28 12:45:19 +0530

    feat(client): created js file for manage department apis

---
+++ Client/src/services/department.js
@@ -0,0 +1,11 @@
+import api from "./api";
+
+const departmentApi = {
+    getAll: (params) => api.get("departments/", params),
+    getById: (id) => api.get(`departments/${id}/`),
+    create: (data) => api.post("departments/", data),
+    update: (id, data) => api.update(`departments/${id}/`, data),
+    delete: (id) => api.delete(`departments/${id}/`),
+};
+
+export default departmentApi;
