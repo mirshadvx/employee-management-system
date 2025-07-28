3ae4ab0e772a4f57ffb50297c30d76e057cf3bb1
mirshad <mirshadkvx@gmail.com>
2025-07-28 09:51:53 +0530

    feat(server): added two extra fields in Department model

--- Server/employee/models.py
+++ Server/employee/models.py
@@ -7,7 +7,9 @@ class Department(models.Model):
     name = models.CharField(max_length=100)
     label = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='departments')
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return f"{self.name} - {self.label}"
     
