aa5c7430e2ad931cdb78a57a43195bc269ec8bf5
mirshad <mirshadkvx@gmail.com>
2025-07-29 21:19:09 +0530

    fix(server): update CORS_ALLOWED_ORIGINS to use environment variable

--- Server/Main/settings.py
+++ Server/Main/settings.py
@@ -13,7 +13,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
-#CORS_ALLOWED_ORIGINS = [ "http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [os.getenv("CORS_ALLOWED_ORIGINS")]
 
 CORS_ALLOW_ALL_ORIGINS = False
 
